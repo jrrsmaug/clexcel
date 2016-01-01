379c223a7698debac67fc954aafe1928f1031d4a
Bernhard Sillipp <bernhard.sillipp@novum-ccservices.at>
2016-01-01 19:15:21 +0100

    added col-seq to get all cells of a column

--- src/clexcel/core.clj
+++ src/clexcel/core.clj
@@ -13,13 +13,13 @@
        (select-columns
         {:B :datum :C :von :D :bis :E :dauer :F :projekt :G :task :H :beschreibung :I :ueberstunden})))
 
-(def here (t/time-zone-for-id "Europe/Vienna"))
+(def tz-here (t/time-zone-for-id "Europe/Vienna"))
 
 (defn month-of
   [entry]
   (t/month
    (tc/to-local-date
-    (t/to-time-zone (tc/to-date-time (:datum entry)) here))))
+    (t/to-time-zone (tc/to-date-time (:datum entry)) tz-here))))
 
 (defn load-month
   [m]
@@ -28,8 +28,7 @@
 
 (defn map-values-to-vec
   [e]
-  (vector
-   (:datum e) (:von e) (:bis e) (:dauer e) (:projekt e) (:task e) (:beschreibung e) (:ueberstunden e)))
+  (row-vec [:datum :von :bis :dauer :projekt :task :beschreibung :ueberstunden] e))
 
 (defn prepare-for-excel
   "takes a vec of maps and prepares it for an excel sheet"
@@ -45,6 +44,11 @@
     (do
       (set-row-style! header-row (create-cell-style! wb {:font {:bold true}}))
       (save-workbook! "2015-05.xlsx" wb))))
+
+(defn col-seq
+  "Returns a sequence of all cells in column col of the sheet"
+  [col sheet]
+  (seq (map #(.getCell % col) (row-seq sheet))))
 
 (defn -main
   "I don't do a whole lot ... yet."
